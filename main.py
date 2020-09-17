ad03d4fd03a6686ed5534069d3a68307c071a7ae
AhnDongwon <lemontech119@gmail.com>
2020-09-17 22:31:51 +0900

    FastAPI 공식문서 공부 (0)

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from typing import Optional
-
+from pydantic import BaseModel
 from fastapi import FastAPI
 
 app = FastAPI()
-
+class Book(BaseModel):
+    name: str
+    author: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
 
 @app.get("/")
 def read_root():
@@ -13,3 +18,13 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/books/{book_id}")
+def search_book(book_id: int):
+    return {"book_id": book_id}
+
+
+@app.post("/books/")
+def create_book(book: Book):
+    return book
