ab84ffbc7ad4fefb72aa936dccb86fe488645914
AhnDongwon <lemontech119@gmail.com>
2020-10-02 20:09:50 +0900

    FastAPI 공식문서 공부 (1)

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
 from typing import Optional
-from pydantic import BaseModel
-from fastapi import FastAPI
+from pydantic import BaseModel, EmailStr
+from fastapi import FastAPI, Header
+
 
 app = FastAPI()
+
+
 class Book(BaseModel):
     name: str
     author: str
     description: Optional[str] = None
     price: float
     tax: Optional[float] = None
+
+
+class UserIn(BaseModel):
+    username: str
+    password: str
+    email: EmailStr
+    full_name: Optional[str] = None
+
+
+class UserOut(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: Optional[str] = None
+
+
+@app.post("/user/", response_model=UserOut)
+async def create_user(user: UserIn):
+    return user
+
 
 @app.get("/")
 def read_root():
@@ -28,3 +50,9 @@ def search_book(book_id: int):
 @app.post("/books/")
 def create_book(book: Book):
     return book
+
+
+@app.get("/headers/")
+def headers(headers_test: Optional[str] = Header(None, convert_underscores=False)):
+    return {"headers_test": headers_test}
+
